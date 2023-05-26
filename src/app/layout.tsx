e7db2875555642ed6834c7d4c30c229a05792989
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-26 09:49:23 -0300

    restart project without back end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import StyledComponentsRegistry from '@/lib/registry'
 import { ReactNode } from 'react'
-import GlobalStyles from '../styles/global'
-
 import { Roboto_Flex as Roboto } from 'next/font/google'
 
 const roboto = Roboto({ subsets: ['latin'] })
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html className={roboto.className}>
       <body>
-        <GlobalStyles />
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
